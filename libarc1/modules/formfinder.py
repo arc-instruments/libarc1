67b2f572e9c572ff6ebc1314f071bea1589040a2
Spyros Stathopoulos <ss2y15@soton.ac.uk>
2020-04-30 15:52:21 +0100

    [FormFinder] Add a generic ramp generator based on FormFinder
    Although FormFinder already provides the capability for programmable
    ramps it is occasionally desirable to have a single ramp generator.
    For that RampGenerator provides a quick alternative to FormFinder
    requiring only voltages, pulse width and interpulse.

--- libarc1/modules/formfinder.py
+++ libarc1/modules/formfinder.py
@@ -78,3 +78,38 @@ class FormFinder(Module):
         pkt = FF_PKT(conf, len(devs))
 
         generic_triplet_runner(instr, pkt, devs, self.sink)
+
+
+class RampGenerator(Module):
+
+    name = "RampGenerator"
+    tag = "RMP"
+    description = """ One-shot ramp generator """
+
+    default_config = {
+            "PWnum": 1,
+            "Vmin": 1.0,
+            "Vstep": 0.25,
+            "Vmax": 1.0,
+            "PW": 100e-6,
+            "interpulse": 10e-3,
+            "pSR": FF_SR.R0 }
+
+    def run(self, devs, conf=default_config):
+        ff_conf = FormFinder.default_config
+        ff_conf["PWnum"] = conf["PWnum"]
+        ff_conf["Vmin"] = conf["Vmin"]
+        ff_conf["Vstep"] = conf["Vstep"]
+        ff_conf["Vmax"] = conf["Vmax"]
+        ff_conf["PWmin"] = conf["PW"]
+        ff_conf["PWmax"] = conf["PW"]
+        ff_conf["PWinter"] = conf["interpulse"]
+        ff_conf["PWstep"] = 100.0
+        ff_conf["Rthr"] = 1.0
+        ff_conf["RthrP"] = 0.0
+        ff_conf["pSR"] = conf["pSR"]
+
+        instr = self.instrument
+        pkt = FF_PKT(ff_conf, len(devs))
+
+        generic_triplet_runner(instr, pkt, devs, self.sink)
